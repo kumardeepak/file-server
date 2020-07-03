4584357ebf0646612b42491538ca84e797a49046
kd <0xbeefdead@gmail.com>
2020-07-03 19:32:24 +0530

    removed unwanted configuration params

--- src/configs/development.py
+++ src/configs/development.py
@@ -1,23 +1,17 @@
-DEBUG               = True
-API_URL_PREFIX      = "/api"
-APP_HOST            = '0.0.0.0'
-APP_PORT            = 5000
-FILE_STORAGE_PATH   = '/Users/kd/Workspace/python/face/data/input' #'/tmp/nginx'
-ENABLE_CORS         = True
+DEBUG                                   = True
+API_URL_PREFIX                          = "/api"
+APP_HOST                                = '0.0.0.0'
+APP_PORT                                = 5000
+FILE_STORAGE_PATH                       = '/Users/kd/Workspace/python/face/data/input' #'/tmp/nginx'
+ENABLE_CORS                             = True
 
-JWT_SECRET_KEY      = 'tarento@ai.com$'
-JWT_ACCESS_TOKEN_EXPIRY_IN_MINS     = 5
-JWT_REFRESH_TOKEN_EXPIRY_IN_DAYS    = 30
-FACE_MATCHING_TOLERANCE             = 0.60000000
+JWT_SECRET_KEY                          = 'tarento@ai.com$'
+JWT_ACCESS_TOKEN_EXPIRY_IN_MINS         = 5
+JWT_REFRESH_TOKEN_EXPIRY_IN_DAYS        = 30
+REDIS_HOSTNAME                          = 'localhost'
+REDIS_PORT                              = 6379
 
-DATABASE_SAVE       = False
-DATABASE_URI        = 'mongodb://localhost:27017'
-DATABASE_NAME       = 'tarento_ai'
-
-REDIS_HOSTNAME      = 'localhost'
-REDIS_PORT          = 6379
-
-SUPPORTED_UPLOAD_FILETYPES = ['application/msword',
+SUPPORTED_UPLOAD_FILETYPES              = ['application/msword',
 'application/pdf',
 'image/x-ms-bmp',
 'image/jpeg',
